5071f4db986e762cd66802fec76b2f3ecc7a42a5
Atmaram <atmaram@bloomsmobility.com>
2019-10-16 11:32:01 +0530

    SDK created.

---
+++ ManchSDK/ManchSDK/ManchSDKManager.swift
@@ -0,0 +1,9 @@
+//
+//  ManchSDKManager.swift
+//  ManchSDK
+//
+//  Created by Atmaram on 10/10/19.
+//  Copyright © 2019 Atmaram. All rights reserved.
+//
+
+import Foundation
---
+++ ManchSDK/ManchSDK/ManchViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ManchViewController.swift
+//  ManchSDK
+//
+//  Created by Atmaram on 10/10/19.
+//  Copyright © 2019 Atmaram. All rights reserved.
+//
+
+import Foundation
